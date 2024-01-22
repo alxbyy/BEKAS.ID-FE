2b4ba6651cf102d766c976c612bbb64ddc7dd015
Alexander Bryan <126944289+alxbyy@users.noreply.github.com>
2024-01-22 11:46:36 +0700

    Delete src directory

--- src/App.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable array-callback-return */
-import "./App.css";
-import Layout from "./component/templates/Layout";
-
-function App() {
-  return (<Layout />);
-}
-
-export default App;
